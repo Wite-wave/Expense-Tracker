15fa97392bd761db39959d3dab4ec40785a750bd
black <caeonrage@gmail.com>
2025-09-24 02:35:22 +0100

    Fixed the issue with cost and figured out how to calculate the total.

--- main.py
+++ main.py
@@ -1,8 +1,11 @@
 import time
+import json
 from data.save import save, load, start
 
 options = []
-costs = [] #I can't figure out how to get the summed prices
+costs = []
+total = 0
+#costs = [] #I can't figure out how to get the summed prices
 #from options
 isdone = None
 mt = ""
@@ -25,23 +28,35 @@ def printAll(array):
     for i in array:
         print(f"{i.get('Name'):^30} {i.get('price'):<10}")
     print(f"{mt:-^80}") #Draw a dilimiter line
-    print(f"{mt:<12}Total: {sum(costs):^30}") #f strings don't
+    print(f"{mt:<12}Total: {getTotal():^30}frs") #f strings don't
     # accept empty string brackets "{}"
+
+def getTotal():
+    total = 0
+    for i in options:
+        total = total + i.get("price")
+    return total
 
 
 def addOptions():
+    total = 0
     isdone = False
     while isdone == False:
         Name = input("Add item: ")
         if(Name.lower() != "done"):
             price = float(input("Price: "))
-            costs.append(price)
             options.append({"Name":Name, "price":price})
+            #getTotal(options)
+
             printAll(options)
+#            printAll(costs)
+# So appararently the little bugger above me was
+# replacing all the numerical variables from
+# cost with the value "i" 
             print("\n")
         else:
             printAll(options)
-            print(f"{sum(costs)}")
+            print(f"{getTotal()}frs")
             print("All done")
             
             isdone = True
@@ -54,19 +69,19 @@ def action():
         action = action.lower()
         if(action == "load"):
             load(options)
-            #print("Loaded!")
+            print("Loaded!")
         elif(action == "save"):
             save(options)
-            #print("Saved!")
+            print("Saved!")
         elif(action == "add"):
-            #print("Added")
             addOptions()
-        elif(action == "stop"):
+            print("Added")
+        elif(action == "exit"):
             active = False
-            print("Bye")
+            print("Bye!")
 
 print(options)
-#start()    
+#start()
 action()
 #addOptions()
 
